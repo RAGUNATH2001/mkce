4cdbb4d01aebd119cc990970a5796c3f7a551783
RAGUNATH <nragunath2018@gmail.com>
2021-12-03 07:34:51 +0530

    committ

---
+++ script.js
@@ -0,0 +1,45 @@
+function showDate1(){
+  // console.log("show date fired")
+    // document.getElementById('myModal').style.display = "block";
+    Swal.fire({
+
+      title: '<strong style = " color:brown; " >ABSTRACT SUBMISSION</strong>',
+      html:  '<h5 id="date">JANUARY 19 2022 </h5>'
+    }
+
+    )
+}
+
+function showDate2(){
+  // console.log("show date fired")
+    // document.getElementById('myModal').style.display = "block";
+    Swal.fire({
+
+      title: '<strong style = " color:brown; " > NOTIFICATION OF ACCEPTANCE</strong>',
+      html:  '<h5 id="date">FEBRUARY 04 2022 </h5>'
+    }
+
+    )
+}
+function showDate3(){
+  // console.log("show date fired")
+    // document.getElementById('myModal').style.display = "block";
+    Swal.fire({
+
+      title: '<strong style = " color:brown; " > FULL PAPER SUBMISSION </strong>',
+      html:  '<h5 id="date">FEBRUARY 14 2022 </h5>'
+    }
+
+    )
+}
+function showDate4(){
+  // console.log("show date fired")
+    // document.getElementById('myModal').style.display = "block";
+    Swal.fire({
+
+      title: '<strong style = " color:brown; " >CONFERENCE DATES</strong>',
+      html:  '<h5 id="date">MARCH 9&10 2022 </h5>'
+    }
+
+    )
+}
